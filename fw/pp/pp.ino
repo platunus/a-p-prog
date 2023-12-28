7c507ecfc656afa375553d62d433e17c88d3942a
hanyazou <hanyazou@gmail.com>
2023-12-28 14:38:05 +0900

    fw: Add OP_READ_ISP_BITS and OP_WRITE_ISP_BITS

--- fw/pp/pp.ino
+++ fw/pp/pp.ino
@@ -5,9 +5,10 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include "fw_pp_ops.h"
+
 // #define DEBUG
 // #define DEBUG_VERBOSE
-#define EXEC_OPS
 #define BAUD	57600	// Baud rate (9600 is default)
 
 #if defined(ARDUINO_AVR_UNO)
@@ -85,7 +86,7 @@ void usart_tx_b(uint8_t data);
 uint8_t usart_rx_rdy(void);
 uint8_t usart_rx_b(void);
 
-#ifdef EXEC_OPS
+#ifdef PP_EXEC_OPS
 void exec_ops(uint8_t *ops, int n);
 #endif
 
@@ -342,7 +343,7 @@ void loop()
         p18qxx_bulk_erase();
         usart_tx_b(0xC3);
         break;
-#ifdef EXEC_OPS
+#ifdef PP_EXEC_OPS
     case 0x80:
         debug_print("0x80: exec_ops: %02x %02x %02x %02x %02x ...",
                     rx_message[1], rx_message[2], rx_message[3], rx_message[4], rx_message[5]);
@@ -1017,13 +1018,46 @@ uint8_t usart_rx_b(void)
 }
 #endif
 
-#ifdef EXEC_OPS
-#include "fw_pp_ops.h"
+#ifdef PP_EXEC_OPS
+uint8_t pp_params[PP_PARAM_NUM_PARAMS] = { 0 };
+
+// #define delay(n) do { uint8_t count = (n); while (0 < count--) _delay_us(10); } while (0)
+#define delay(n) do { delayMicroseconds(n); } while (0)
+// #define CLK_DELAY delay(pp_params[PP_PARAM_CLK_DELAY])
+#define CLK_DELAY DELAY
+
+void send_bits(uint8_t d, uint8_t len)
+{
+    if (len)
+        verbose_print("%s: %02x (%d bits)", __func__, d, len);
+    ISP_DAT_OUT;
+    for (int i = 0; i < len; i++) {
+        ISP_DAT(d & 0x80);
+        CLK_DELAY;
+        ISP_CLK(1);
+        CLK_DELAY;
+        d <<= 1;
+        ISP_CLK(0);
+        ISP_DAT(0);
+    }
+}
+
+void dummy_locks(uint8_t n)
+{
+    for (int i = 0; i < n; i++) {
+        CLK_DELAY;
+        ISP_CLK(1);
+        CLK_DELAY;
+        ISP_CLK(0);
+    }
+}
 
 void exec_ops(uint8_t *ops, int len)
 {
     uint8_t i, n, t, d;
     uint8_t *endp = &ops[len];
+    uint8_t txbuf[4];
+    uint8_t txbuf_len;
 
     while (ops < endp) {
         n = ops[1];
@@ -1051,7 +1085,8 @@ void exec_ops(uint8_t *ops, int len)
             }
             break;
         case OP_READ_ISP:
-            verbose_print("0x80: exec_ops: %13s %02x %02x", "READ_ISP", n, ops[2]);
+            verbose_print("0x80: exec_ops: %13s %02x %02x %02x %02x", "READ_ISP", n,
+                          ops[2], ops[3], ops[4]);
             d = 0;
             ISP_DAT_IN;
             while (0 < n--) {
@@ -1066,7 +1101,8 @@ void exec_ops(uint8_t *ops, int len)
             }
             break;
         case OP_WRITE_ISP:
-            verbose_print("0x80: exec_ops: %13s %02x %02x", "WRITE_ISP", n, ops[2]);
+            verbose_print("0x80: exec_ops: %13s %02x %02x %02x %02x", "WRITE_ISP", n,
+                          ops[2], ops[3], ops[4]);
             ISP_DAT_OUT;
             while (0 < n--) {
                 d = ops[2];
@@ -1083,14 +1119,74 @@ void exec_ops(uint8_t *ops, int len)
             }
             DELAY3;  // XXX
             break;
+#ifdef PP_EXEC_OPS_RW_BITS
         case OP_READ_ISP_BITS:
             verbose_print("0x80: exec_ops: %13s %02x %02x", "READ_ISP_BITS", n, ops[2]);
-            // not implemented yet
+            while (0 < n--) {
+
+                send_bits(pp_params[PP_PARAM_CMD1], pp_params[PP_PARAM_CMD1_LEN]);
+                delay(pp_params[PP_PARAM_DELAY1]);
+
+                send_bits(pp_params[PP_PARAM_CMD2], pp_params[PP_PARAM_CMD2_LEN]);
+                delay(pp_params[PP_PARAM_DELAY2]);
+
+                ISP_DAT_IN;
+                dummy_locks(pp_params[PP_PARAM_PREFIX_LEN]);
+                d = 0;
+                txbuf_len = 0;
+                for (i = 0; i < pp_params[PP_PARAM_DATA_LEN]; i++) {
+                    CLK_DELAY;
+                    ISP_CLK(1);
+                    CLK_DELAY;
+                    ISP_CLK(0);
+                    d = (d << 1) | ((ISP_DAT_V) ? 1 : 0);
+                    if ((i % 8) == 7) {
+                        txbuf[txbuf_len++] = d;
+                        d = 0;
+                    }
+                }
+                if ((i % 8)) {
+                    txbuf[txbuf_len++] = d;
+                }
+                dummy_locks(pp_params[PP_PARAM_POSTFIX_LEN]);
+                delay(pp_params[PP_PARAM_DELAY3]);
+                for (i = 0; i < txbuf_len; i++) {
+                    verbose_print("0x80: exec_ops: %13s tx: %02x %02x", "READ_ISP_BITS", n, txbuf[i]);
+                    usart_tx_b(txbuf[i]);
+                }
+            }
             break;
         case OP_WRITE_ISP_BITS:
             verbose_print("0x80: exec_ops: %13s %02x %02x", "WRITE_ISP_BITS", n, ops[2]);
-            // not implemented yet
+            while (0 < n--) {
+
+                send_bits(pp_params[PP_PARAM_CMD1], pp_params[PP_PARAM_CMD1_LEN]);
+                delay(pp_params[PP_PARAM_DELAY1]);
+
+                send_bits(pp_params[PP_PARAM_CMD2], pp_params[PP_PARAM_CMD2_LEN]);
+                delay(pp_params[PP_PARAM_DELAY2]);
+
+                ISP_DAT(0);
+                dummy_locks(pp_params[PP_PARAM_PREFIX_LEN]);
+                for (i = 0; i < pp_params[PP_PARAM_DATA_LEN]; i++) {
+                    if ((i % 8) == 0) {
+                        d = ops[2];
+                        verbose_print("0x80: exec_ops: %13s rx: %02x %02x", "WRITE_ISP_BITS", n, d);
+                        ops++;
+                    }
+                    ISP_DAT(d & 0x80);
+                    d <<= 1;
+                    CLK_DELAY;
+                    ISP_CLK(1);
+                    CLK_DELAY;
+                    ISP_CLK(0);
+                }
+                ISP_DAT(0);
+                dummy_locks(pp_params[PP_PARAM_POSTFIX_LEN]);
+                delay(pp_params[PP_PARAM_DELAY3]);
+            }
             break;
+#endif  // PP_EXEC_OPS_RW_BITS
         case OP_DELAY_US:
             verbose_print("0x80: exec_ops: %13s %02x", "DELAY_US", n);
             while (0 < n--)
@@ -1110,6 +1206,16 @@ void exec_ops(uint8_t *ops, int len)
             verbose_print("0x80: exec_ops: %13s %02x", "REPLAY", n);
             usart_tx_b(n);
             break;
+        case OP_PARAM_SET:
+            verbose_print("0x80: exec_ops: %13s pp_params[%02x] = %02x", "PARAM_SET", n, ops[2]);
+            pp_params[n] = ops[2];
+            ops++;
+            break;
+        case OP_PARAM_RESET:
+            verbose_print("0x80: exec_ops: %13s", "PARAM_RESET", n);
+            memset(pp_params, 0, sizeof(pp_params));
+            ops -= 1;  // no operands
+            break;
         default:
             verbose_print("0x80: exec_ops: %13s %02x", "???", n);
             break;
@@ -1117,4 +1223,4 @@ void exec_ops(uint8_t *ops, int len)
         ops += 2;
     }
 }
-#endif
+#endif  // PP_EXEC_OPS
